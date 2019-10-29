b4be840c477fc398e09d488adda04f1f33320919
Michiel de Vos <Michiel_87@hotmail.com>
2019-10-29 22:10:49 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sqlite3.h> 
+#include <stdlib.h>
+
+int main(void) 
+{
+        sqlite3 *db;
+
+        char *zErrMsg = 0;
+        int status;
+
+        status = sqlite3_open("test.db", &db);
+        
+        if (status != 0) {
+                fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+                printf("Status: %d\n", status);
+                exit(0);
+        } else {
+                fprintf(stderr, "Opened database successfully\n");
+                printf("Status: %d\n", status);
+        }
+
+        sqlite3_close(db);
+        return 0;
+}
